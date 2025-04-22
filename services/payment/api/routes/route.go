00b074d8fcce43a35e86315e06d91456fff12c38
unknown <ovieighosuakpo@gmail.com>
2025-04-22 14:14:15 +0100

    fixes

---
+++ services/payment/api/routes/route.go
@@ -0,0 +1,20 @@
+package routes
+
+import (
+	"github.com/Creative-genius001/user/api/handlers"
+	"github.com/gin-gonic/gin"
+)
+
+func InitializeRoutes(router *gin.Engine) {
+
+	userRouter := router.Group("/api/user")
+	{
+		userRouter.GET("/:id", handlers.GetUserData)
+	}
+
+	authRouter := router.Group("/api/auth")
+	{
+		authRouter.POST("/login", handlers.Login)
+		authRouter.POST("/register", handlers.Register)
+	}
+}
---
+++ services/payment/utils/logger.go
@@ -0,0 +1,90 @@
+package utils
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
+
+var logger = logrus.New()
+
+func init() {
+	logger.Level = logrus.InfoLevel
+	logger.Formatter = &formatter{}
+
+	logger.SetReportCaller(true)
+}
+
+func SetLogLevel(level logrus.Level) {
+	logger.Level = level
+}
+
+type Fields logrus.Fields
+
+func Debug(args ...interface{}) {
+	if logger.Level >= logrus.DebugLevel {
+		logger.Debug(fmt.Sprint(args...))
+	}
+}
+
+func Info(args ...interface{}) {
+	if logger.Level >= logrus.InfoLevel {
+		logger.Info(fmt.Sprint(args...))
+	}
+}
+
+func Warn(args ...interface{}) {
+	if logger.Level >= logrus.WarnLevel {
+		logger.Warn(fmt.Sprint(args...))
+	}
+}
+
+func Error(args ...interface{}) {
+	if logger.Level >= logrus.ErrorLevel {
+		logger.Error(fmt.Sprint(args...))
+	}
+}
+
+func Fatal(args ...interface{}) {
+	if logger.Level >= logrus.FatalLevel {
+		logger.Fatal(fmt.Sprint(args...))
+	}
+}
+
+// ANSI color codes
+const (
+	colorReset  = "\033[0m"
+	colorRed    = "\033[31m"
+	colorGreen  = "\033[32m"
+	colorYellow = "\033[33m"
+)
+
+// Formatter implements logrus.Formatter interface.
+type formatter struct{}
+
+// Format building log message.
+func (f *formatter) Format(entry *logrus.Entry) ([]byte, error) {
+	var sb bytes.Buffer
+	var color string
+	switch entry.Level {
+	case logrus.InfoLevel:
+		color = colorGreen
+	case logrus.WarnLevel:
+		color = colorYellow
+	case logrus.ErrorLevel, logrus.FatalLevel:
+		color = colorRed
+	default:
+		color = colorReset
+	}
+
+	timestamp := entry.Time.Format(time.RFC3339)
+	level := strings.ToUpper(entry.Level.String())
+
+	logLine := fmt.Sprintf("%s[%s] %s %s%s\n", color, level, timestamp, entry.Message, colorReset)
+	sb.WriteString(logLine)
+
+	return sb.Bytes(), nil
+}
